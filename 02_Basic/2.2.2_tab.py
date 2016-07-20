27c915804e41c08372479711267f7538f3b8f3f1
Yih Jie <yihjie@gmail.com>
2016-07-20 22:59:44 +0800

    零基礎學Python  Chapter 2 Python語言基礎

---
+++ 02_Basic/2.2.2_tab.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+x = 1
+if x == 1 :
+    print("x = " , x)
+else:
+    print("x = " , x)
+x = x + 1
+print("x = " , x)
---
+++ 02_Basic/2.2.3_from_import.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from sys import argv
+from sys import path
+
+print(path)
+print(argv)
